a25d5cd97f55b07da58cee75a9c5b78abfdcfb50
Matheus <matheusprofess@gmail.com>
2024-11-22 15:22:20 -0300

    rbac actually working

--- app/Http/Middleware/AccessControlMiddleware.php
+++ app/Http/Middleware/AccessControlMiddleware.php
@@ -14,7 +14,7 @@ class AccessControlMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, string $roleName, string $method): Response
+    public function handle(Request $request, Closure $next, string $roleName): Response
     {
         if (!Auth::check()) {
             return response()->json(['message' => 'Unauthorized.'], 401);
--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('/sign', [UserController::class, 'sign'])->name('user.sign');
 
 Route::middleware('auth:sanctum')->group(function() {
-  Route::get('/intentions/read', [IntentionController::class, 'readAll'])->middleware('access.control:admin,read');
+  Route::get('/intentions/read', [IntentionController::class, 'readAll'])->middleware('access.control:admin');
 });
