cebc96563ccb520d4557bc98d9e6bb1868d4353f
Matheus <matheusprofess@gmail.com>
2024-12-18 17:05:27 -0300

    trying not succeeded livewire pagination

--- app/Livewire/Pages/IntentionsDetails.php
+++ app/Livewire/Pages/IntentionsDetails.php
@@ -4,6 +4,7 @@ namespace App\Livewire\Pages;
 
 use App\Models\Church;
 use App\Models\Intention;
+use Carbon\Carbon;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -11,24 +12,38 @@ class IntentionsDetails extends Component
 {
     use WithPagination;
     public $urlDate = "";
-    public $intentionsGroup = [];
-    public $massHours = ['07:00','08:00','09:00','10:00','18:00','19:00'];
-    public $churches = []; 
-    public $selectedHour = '07:00';
-    public $selectedChurch = 1;        
+    public $massHours = ['07:00', '08:00', '09:00', '10:00', '18:00', '19:00'];
+    public $churches = [];
+    public $selectedHour = '08:00';
+    public $selectedChurch = 1;
 
-    public function filterIntentions() {}
+    public function filterIntentions(string $hour = null, string $churchId = null)
+    {
+        $this->selectedHour = $hour ?? $this->selectedHour;
+        $this->selectedChurch = $churchId ?? $this->selectedChurch;
 
-    public function mount($date) {
-        $this->intentionsGroup = Intention::whereDate('mass_date', $date)->get();
+        $this->resetPage();
+    }
+
+    public function mount($date)
+    {
         $this->urlDate = $date;
         $this->churches = Church::all();
     }
 
     public function render()
     {
-        return view('livewire.pages.intentions-details')
-            ->extends('components.layouts.app', ['title' => 'Intenções | Detalhes'])
+        $date = Carbon::parse($this->urlDate);
+
+        $intentionsGroup = Intention::whereDate('mass_date', $date)
+            ->whereTime('mass_date', $this->selectedHour)
+            ->where('church_id', $this->selectedChurch)
+            ->paginate(5);
+
+        return view('livewire.pages.intentions-details', [
+            'intentionsGroup' => $intentionsGroup,
+        ])
+            ->extends('components.layouts.app')
             ->section('content');
     }
 }
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,6 +7,8 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Poppins:wght@100..900&display=swap" rel="stylesheet">
+  @livewireStyles
+  @livewireScripts
   @vite(['resources/js/app.js', 'resources/css/app.css'])
   <title>@yield('title')</title>
 </head>
